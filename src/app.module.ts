c56d9f3fb66dc99c0a71d97a024c55ee80103857
denis_levchenko <tranzi90@gmail.com>
2024-01-31 20:54:03 +0300

    test connection string

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'example',
+      database: 'nest-events',
+    }),
+  ],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
